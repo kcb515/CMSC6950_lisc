f6813193d5cbc980fb185b816a160540a18438fe
Katie Brown <kcb515@mun.ca>
2020-08-05 14:19:46 -0230

    Made figure larger

--- plot3.py
+++ plot3.py
@@ -2,10 +2,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-terms = ['Salmonella enterica','Escherichia coli','Sus scrofa','Homo sapiens','Mus musculus']
+terms = ['Escherichia coli','Sus scrofa','Homo sapiens','Mus musculus','Salmonella enterica',]
 plt.bar(terms, [2940420,11455876,3751565,47509715,11515320])
 plt.xlabel('Organism')
 plt.ylabel('Number of Articles')
 plt.title('The number of Articles found for each Organism')
+plt.figure(figsize=(20,10))
 plt.show()
 plt.savefig('organism.png')
