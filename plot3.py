34c59b7d534afb1efac9fd9e1c6ad95aa00b7113
Katie Brown <kcb515@mun.ca>
2020-08-06 14:28:02 -0230

    Last change

--- plot3.py
+++ plot3.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-terms = ['Salmonella','Sus scrofa','Homo sapiens','Mus musculus','E.coli']
+terms = ['Salmonella','Sus scrofa','Mus musculus','Homo sapiens','E.coli']
 plt.bar(terms, [2940420,11455876,3751565,47509715,11515320])
 plt.xlabel('Organism')
 plt.ylabel('Number of Articles')
